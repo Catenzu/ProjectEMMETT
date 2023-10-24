6df72ed6910b3e4206333b5552eddca468bbe8f7
Catenzu <benjamin@bourge.be>
2023-10-24 11:49:23 +0200

    AND testing

--- src/AND.cpp
+++ src/AND.cpp
@@ -9,7 +9,7 @@
 
 void MOS6502::AND_IM(int &cycles)
 {
-    unsigned char value = fetch(cycles);
+    unsigned char value = getValueImmediate(cycles);
     unsigned char result = _a._value & value;
 
     setNegativeFlag(result);
@@ -20,8 +20,7 @@ void MOS6502::AND_IM(int &cycles)
 
 void MOS6502::AND_ZERO(int &cycles)
 {
-    unsigned char address = fetch(cycles);
-    unsigned char value = getMemory(address, cycles);
+    unsigned char value = getValueZeroPage(cycles);
     unsigned char result = _a._value & value;
 
     setNegativeFlag(result);
@@ -32,11 +31,8 @@ void MOS6502::AND_ZERO(int &cycles)
 
 void MOS6502::AND_ZEROX(int &cycles)
 {
-    unsigned char address = fetch(cycles);
-    address += _x._value;
-    unsigned char value = getMemory(address, cycles);
+    unsigned char value = getValueZeroPageX(cycles);
     unsigned char result = _a._value & value;
-    cycles--;
 
     setNegativeFlag(result);
     setZeroFlag(result);
@@ -46,10 +42,8 @@ void MOS6502::AND_ZEROX(int &cycles)
 
 void MOS6502::AND_ABS(int &cycles)
 {
-    uint16_t address = fetch(cycles);
-    unsigned char value = getMemory(address, cycles);
+    unsigned char value = getValueAbsolute(cycles);
     unsigned char result = _a._value & value;
-    cycles--;
 
     setNegativeFlag(result);
     setZeroFlag(result);
@@ -59,29 +53,19 @@ void MOS6502::AND_ABS(int &cycles)
 
 void MOS6502::AND_ABSX(int &cycles)
 {
-    unsigned char addressLow = fetch(cycles);
-    unsigned char addressHigh = fetch(cycles);
-    uint16_t address = ((uint16_t)addressHigh * 256) + (uint16_t)addressLow;
-    address += _x._value;
-    bool pageCrossed = (address & 0xFF00) != (addressHigh << 8);
-    unsigned char value = getMemory(address, cycles);
+    unsigned char value = getValueAbsoluteX(cycles);
     unsigned char result = _a._value & value;
 
     setZeroFlag(result);
     setNegativeFlag(result);
     _a.set(result);
-    if (pageCrossed)
-        cycles++;
     std::cout << "--AND ABSX: " << (int) _a._value << std::endl;
 }
 
 void MOS6502::AND_ABSY(int &cycles)
 {
-    uint16_t address = fetch(cycles);
-    address += _y._value;
-    unsigned char value = getMemory(address, cycles);
+    unsigned char value = getValueAbsoluteY(cycles);
     unsigned char result = _a._value & value;
-    cycles--;
 
     setNegativeFlag(result);
     setZeroFlag(result);
@@ -91,10 +75,22 @@ void MOS6502::AND_ABSY(int &cycles)
 
 void MOS6502::AND_INDX(int &cycles)
 {
+    unsigned char value = getValueIndirectX(cycles);
+    unsigned char result = _a._value & value;
 
+    setNegativeFlag(result);
+    setZeroFlag(result);
+    _a.set(result);
+    std::cout << "--AND Indirect X: " << (int) _a._value << std::endl;
 }
 
 void MOS6502::AND_INDY(int &cycles)
 {
+    unsigned char value = getValueIndirectY(cycles);
+    unsigned char result = _a._value & value;
 
+    setNegativeFlag(result);
+    setZeroFlag(result);
+    _a.set(result);
+    std::cout << "--AND Indirect Y: " << (int) _a._value << std::endl;
 }
--- src/unitTests/testAND.cpp
+++ src/unitTests/testAND.cpp
@@ -21,7 +21,7 @@ void test_AND(MOS6502 &cpu)
     if ((cyclesUsed = cpu.execute(2)) != 0)
         std::cerr << "FAILURE: AND Immediate cycles Used: " << cyclesUsed << " != 0" << std::endl;
     if (cpu._a._value != 0b00000011) {
-        std::cerr << "FAILURE: AND Immediate: " << (int) cpu._a._value << " != 0x0A" << std::endl;
+        std::cerr << "FAILURE: AND Immediate: " << (int) cpu._a._value << " != 0b00000011" << std::endl;
     } else
         std::cout << "SUCCESS: AND Immediate: " << (int) cpu._a._value << std::endl;
     cpu.clear();
@@ -39,7 +39,7 @@ void test_AND(MOS6502 &cpu)
     if ((cyclesUsed = cpu.execute(3)) != 0)
         std::cerr << "FAILURE: AND Zero Page cycles Used: " << cyclesUsed << " != 0" << std::endl;
     if (cpu._a._value != 0b00000011) {
-        std::cerr << "FAILURE: AND Zero Page: " << (int) cpu._a._value << " != 0x10" << std::endl;
+        std::cerr << "FAILURE: AND Zero Page: " << (int) cpu._a._value << " != 0b00000011" << std::endl;
     } else
         std::cout << "SUCCESS: AND Zero Page: " << (int) cpu._a._value << std::endl;
     cpu.clear();
@@ -59,7 +59,7 @@ void test_AND(MOS6502 &cpu)
     if ((cyclesUsed = cpu.execute(4)) != 0)
         std::cerr << "FAILURE: AND Zero Page X cycles Used: " << cyclesUsed << " != 0" << std::endl;
     if (cpu._a._value != 0b00000011) {
-        std::cerr << "FAILURE: AND Zero Page X: " << (int) cpu._a._value << " != 0x10" << std::endl;
+        std::cerr << "FAILURE: AND Zero Page X: " << (int) cpu._a._value << " != 0b00000011" << std::endl;
     } else {
         std::cout << "SUCCESS: AND Zero Page X: " << (int) cpu._a._value << std::endl;
     }
@@ -79,7 +79,7 @@ void test_AND(MOS6502 &cpu)
     if ((cyclesUsed = cpu.execute(4)) != 0)
         std::cerr << "FAILURE: AND Absolute cycles Used: " << cyclesUsed << " != 0" << std::endl;
     if (cpu._a._value != 0b00000011) {
-        std::cerr << "FAILURE: AND Absolute: " << (int) cpu._a._value << " != 0x10" << std::endl;
+        std::cerr << "FAILURE: AND Absolute: " << (int) cpu._a._value << " != 0b00000011" << std::endl;
     } else
         std::cout << "SUCCESS: AND Absolute: " << (int) cpu._a._value << std::endl;
     cpu.clear();
@@ -101,7 +101,7 @@ void test_AND(MOS6502 &cpu)
     if ((cyclesUsed = cpu.execute(4)) != 0)
         std::cerr << "FAILURE: AND Absolute X cycles Used: " << cyclesUsed << " != 0" << std::endl;
     if (cpu._a._value != 0b00000011) {
-        std::cerr << "FAILURE: AND Absolute X: " << (int) cpu._a._value << " != 0x10" << std::endl;
+        std::cerr << "FAILURE: AND Absolute X: " << (int) cpu._a._value << " != 0b00000011" << std::endl;
     } else
         std::cout << "SUCCESS: AND Absolute X: " << (int) cpu._a._value << std::endl;
     cpu.clear();
@@ -123,13 +123,50 @@ void test_AND(MOS6502 &cpu)
     if ((cyclesUsed = cpu.execute(4)) != 0)
         std::cerr << "FAILURE: AND Absolute Y cycles Used: " << cyclesUsed << " != 0" << std::endl;
     if (cpu._a._value != 0b00000011) {
-        std::cerr << "FAILURE: AND Absolute Y: " << (int) cpu._a._value << " != 0x10" << std::endl;
+        std::cerr << "FAILURE: AND Absolute Y: " << (int) cpu._a._value << " != 0b00000011" << std::endl;
     } else
         std::cout << "SUCCESS: AND Absolute Y: " << (int) cpu._a._value << std::endl;
     cpu.clear();
 
     //indirect x
 
+    cpu._memory[0xFFFC]._value = 0x00;
+    cpu._memory[0xFFFD]._value = 0x10;
+    cpu._memory[0x1000]._value = 0x21;
+    cpu._memory[0x1001]._value = 0x05;
+    cpu._memory[0x0007]._value = 0x0B;
+    cpu._memory[0x0008]._value = 0x00;
+    cpu._memory[0x000B]._value = 0b00001111;
+    cpu.reset();
+    cpu._a._value = 0b00110011;
+    cpu._x._value = 0x02;
+
+    if ((cyclesUsed = cpu.execute(6)) != 0)
+        std::cerr << "FAILURE: AND Indirect X cycles Used: " << cyclesUsed << " != 0" << std::endl;
+    if (cpu._a._value != 0b00000011) {
+        std::cerr << "FAILURE: AND Indirect X: " << (int) cpu._a._value << " != 0b00000011" << std::endl;
+    } else
+        std::cout << "SUCCESS: AND Indirect X: " << (int) cpu._a._value << std::endl;
+    cpu.clear();
+
     //indirect y
 
+    cpu._memory[0xFFFC]._value = 0x00;
+    cpu._memory[0xFFFD]._value = 0x10;
+    cpu._memory[0x1000]._value = 0x31;
+    cpu._memory[0x1001]._value = 0x05;
+    cpu._memory[0x0005]._value = 0x0B;
+    cpu._memory[0x0006]._value = 0x00;
+    cpu._memory[0x000D]._value = 0b00001111;
+    cpu.reset();
+    cpu._a._value = 0b00110011;
+    cpu._y._value = 0x02;
+
+    if ((cyclesUsed = cpu.execute(5)) != 0)
+        std::cerr << "FAILURE: AND Indirect Y cycles Used: " << cyclesUsed << " != 0" << std::endl;
+    if (cpu._a._value != 0b00000011) {
+        std::cerr << "FAILURE: AND Indirect Y: " << (int) cpu._a._value << " != 0b00000011" << std::endl;
+    } else
+        std::cout << "SUCCESS: AND Indirect Y: " << (int) cpu._a._value << std::endl;
+    cpu.clear();
 }
