ab5fe6475605e18486654ad3ad391968072811fa
Catenzu <benjamin@bourge.be>
2023-11-01 14:09:59 +0100

    CMP

--- src/CMP.cpp
+++ src/CMP.cpp
@@ -8,78 +8,133 @@ void MOS6502::CMP_IM(int &cycles)
 {
     unsigned char value = getValueImmediate(cycles);
 
-    if (_a._value >= value)
+    if (_a._value >= value) //CARRY IF A >= VALUE
         _sr.set(_sr._value | 0b00000001);
     else
         _sr.set(_sr._value & 0b11111110);
-    if (_a._value == value)
+
+    if (_a._value == value) //ZERO IF A == VALUE
         _sr.set(_sr._value | 0b00000010);
     else
         _sr.set(_sr._value & 0b11111101);
 
     unsigned char result = _a._value - value;
     setNegativeFlagFromByte(result);
-    std::cout << "--CMP Immediate: " << (int) _a._value << std::endl;
+    std::cout << "--CMP Immediate: " << (int) result << std::endl;
 }
 
 void MOS6502::CMP_ZERO(int &cycles)
 {
     unsigned char value = getValueZeroPage(cycles);
 
-    _a.set(value);
-    setNegativeFlagFromByte(value);
-    setZeroFlagFromByte(value);
-    std::cout << "--CMP Zero Page: " << (int) _a._value << std::endl;
+    if (_a._value >= value) //CARRY IF A >= VALUE
+        _sr.set(_sr._value | 0b00000001);
+    else
+        _sr.set(_sr._value & 0b11111110);
+
+    if (_a._value == value) //ZERO IF A == VALUE
+        _sr.set(_sr._value | 0b00000010);
+    else
+        _sr.set(_sr._value & 0b11111101);
+
+    unsigned char result = _a._value - value;
+    setNegativeFlagFromByte(result);
+    std::cout << "--CMP Zero Page: " << (int) result << std::endl;
 }
 
 void MOS6502::CMP_ZEROX(int &cycles)
 {
     unsigned char value = getValueZeroPageX(cycles);
 
-    _a.set(value);
-    setNegativeFlagFromByte(value);
-    setZeroFlagFromByte(value);
-    std::cout << "--CMP ZeroX Page: " << (int) _a._value << std::endl;
+    if (_a._value >= value) //CARRY IF A >= VALUE
+        _sr.set(_sr._value | 0b00000001);
+    else
+        _sr.set(_sr._value & 0b11111110);
+
+    if (_a._value == value) //ZERO IF A == VALUE
+        _sr.set(_sr._value | 0b00000010);
+    else
+        _sr.set(_sr._value & 0b11111101);
+
+    unsigned char result = _a._value - value;
+    setNegativeFlagFromByte(result);
+    std::cout << "--CMP ZeroX Page: " << (int) result << std::endl;
 }
 
 void MOS6502::CMP_ABS(int &cycles)
 {
     unsigned char value = getValueAbsolute(cycles);
 
-    _a.set(value);
-    setNegativeFlagFromByte(value);
-    setZeroFlagFromByte(value);
-    std::cout << "--CMP Absolute: " << (int) _a._value << std::endl;
+    if (_a._value >= value) //CARRY IF A >= VALUE
+        _sr.set(_sr._value | 0b00000001);
+    else
+        _sr.set(_sr._value & 0b11111110);
+
+    if (_a._value == value) //ZERO IF A == VALUE
+        _sr.set(_sr._value | 0b00000010);
+    else
+        _sr.set(_sr._value & 0b11111101);
+
+    unsigned char result = _a._value - value;
+    setNegativeFlagFromByte(result);
+    std::cout << "--CMP Absolute: " << (int) result << std::endl;
 }
 
 void MOS6502::CMP_ABSX(int &cycles)
 {
     unsigned char value = getValueAbsoluteX(cycles);
 
-    _a.set(value);
-    setNegativeFlagFromByte(value);
-    setZeroFlagFromByte(value);
-    std::cout << "--CMP Absolute X: " << (int) _a._value << std::endl;
+    if (_a._value >= value) //CARRY IF A >= VALUE
+        _sr.set(_sr._value | 0b00000001);
+    else
+        _sr.set(_sr._value & 0b11111110);
+
+    if (_a._value == value) //ZERO IF A == VALUE
+        _sr.set(_sr._value | 0b00000010);
+    else
+        _sr.set(_sr._value & 0b11111101);
+
+    unsigned char result = _a._value - value;
+    setNegativeFlagFromByte(result);
+    std::cout << "--CMP Absolute X: " << (int) result << std::endl;
 }
 
 void MOS6502::CMP_ABSY(int &cycles)
 {
     unsigned char value = getValueAbsoluteY(cycles);
 
-    _a.set(value);
-    setNegativeFlagFromByte(value);
-    setZeroFlagFromByte(value);
-    std::cout << "--CMP Absolute Y: " << (int) _a._value << std::endl;
+    if (_a._value >= value) //CARRY IF A >= VALUE
+        _sr.set(_sr._value | 0b00000001);
+    else
+        _sr.set(_sr._value & 0b11111110);
+
+    if (_a._value == value) //ZERO IF A == VALUE
+        _sr.set(_sr._value | 0b00000010);
+    else
+        _sr.set(_sr._value & 0b11111101);
+
+    unsigned char result = _a._value - value;
+    setNegativeFlagFromByte(result);
+    std::cout << "--CMP Absolute Y: " << (int) result << std::endl;
 }
 
 void MOS6502::CMP_INDX(int &cycles)
 {
     unsigned char value = getValueIndirectX(cycles);
 
-    _a.set(value);
-    setNegativeFlagFromByte(value);
-    setZeroFlagFromByte(value);
-    std::cout << "--CMP Indirect X: " << (int) _a._value << std::endl;
+    if (_a._value >= value) //CARRY IF A >= VALUE
+        _sr.set(_sr._value | 0b00000001);
+    else
+        _sr.set(_sr._value & 0b11111110);
+
+    if (_a._value == value) //ZERO IF A == VALUE
+        _sr.set(_sr._value | 0b00000010);
+    else
+        _sr.set(_sr._value & 0b11111101);
+
+    unsigned char result = _a._value - value;
+    setNegativeFlagFromByte(result);
+    std::cout << "--CMP Indirect X: " << (int) result << std::endl;
 }
 
 void MOS6502::CMP_INDY(int &cycles)
@@ -88,6 +143,17 @@ void MOS6502::CMP_INDY(int &cycles)
 
     _a.set(value);
     setNegativeFlagFromByte(value);
-    setZeroFlagFromByte(value);
-    std::cout << "--CMP Indirect Y: " << (int) _a._value << std::endl;
+    setZeroFlagFromByte(value);if (_a._value >= value) //CARRY IF A >= VALUE
+        _sr.set(_sr._value | 0b00000001);
+    else
+        _sr.set(_sr._value & 0b11111110);
+
+    if (_a._value == value) //ZERO IF A == VALUE
+        _sr.set(_sr._value | 0b00000010);
+    else
+        _sr.set(_sr._value & 0b11111101);
+
+    unsigned char result = _a._value - value;
+    setNegativeFlagFromByte(result);
+    std::cout << "--CMP Indirect Y: " << (int) result << std::endl;
 }
--- src/unitTests/testCMP.cpp
+++ src/unitTests/testCMP.cpp
@@ -2,12 +2,192 @@
  * Emmett
  */
 
+#include <bitset>
 #include "../../include/emmett.h"
 
 int test_CMP(MOS6502 &cpu)
 {
     int cyclesUsed = 0;
 
+    // CMP Immediate a = m
 
+    cpu.clear();
+    cpu._memory[0xFFFC]._value = 0x00;
+    cpu._memory[0xFFFD]._value = 0x10;
+    cpu._memory[0x1000]._value = 0xC9;
+    cpu._memory[0x1001]._value = 0x05;
+
+    cpu.reset();
+    cpu._a.set(0x05);
+
+    if ((cyclesUsed = cpu.execute(2)) != 0)
+        std::cerr << "FAILURE: CMP Immediate cycles Used: " << cyclesUsed << " != 0" << std::endl;
+    if (cpu._sr._value != 0b00000011) {
+        std::cerr << "FAILURE: CMP Immediate: " << std::bitset<8>(cpu._sr._value) << " != 0b00000011" << std::endl;
+    } else
+        std::cout << "SUCCESS: CMP Immediate: " << std::bitset<8>(cpu._sr._value) << std::endl;
+
+    // CMP Immediate a > m
+
+    cpu.clear();
+    cpu._memory[0xFFFC]._value = 0x00;
+    cpu._memory[0xFFFD]._value = 0x10;
+    cpu._memory[0x1000]._value = 0xC9;
+    cpu._memory[0x1001]._value = 0x05;
+
+    cpu.reset();
+    cpu._a.set(0x10);
+
+    if ((cyclesUsed = cpu.execute(2)) != 0)
+        std::cerr << "FAILURE: CMP Immediate cycles Used: " << cyclesUsed << " != 0" << std::endl;
+    if (cpu._sr._value != 0b00000001) {
+        std::cerr << "FAILURE: CMP Immediate: " << std::bitset<8>(cpu._sr._value) << " != 0b00000001" << std::endl;
+    } else
+        std::cout << "SUCCESS: CMP Immediate: " << std::bitset<8>(cpu._sr._value) << std::endl;
+
+    // CMP Zero Page
+
+    cpu.clear();
+    cpu._memory[0xFFFC]._value = 0x00;
+    cpu._memory[0xFFFD]._value = 0x10;
+    cpu._memory[0x1000]._value = 0xC5;
+    cpu._memory[0x1001]._value = 0x05;
+    cpu._memory[0x0005]._value = 0x10;
+
+    cpu.reset();
+    cpu._a.set(0x10);
+
+    if ((cyclesUsed = cpu.execute(3)) != 0)
+        std::cerr << "FAILURE: CMP Zero Page cycles Used: " << cyclesUsed << " != 0" << std::endl;
+    if (cpu._sr._value != 0b00000011) {
+        std::cerr << "FAILURE: CMP Zero Page: " << std::bitset<8>(cpu._sr._value) << " != 0b00000011" << std::endl;
+    } else
+        std::cout << "SUCCESS: CMP Zero Page: " << std::bitset<8>(cpu._sr._value) << std::endl;
+
+    // CMP Zero Page X
+
+    cpu.clear();
+    cpu._memory[0xFFFC]._value = 0x00;
+    cpu._memory[0xFFFD]._value = 0x10;
+    cpu._memory[0x1000]._value = 0xD5;
+    cpu._memory[0x1001]._value = 0x02;
+    cpu._memory[0x0005]._value = 0x10;
+
+    cpu.reset();
+    cpu._x.set(0x03);
+    cpu._a.set(0x10);
+
+    if ((cyclesUsed = cpu.execute(4)) != 0)
+        std::cerr << "FAILURE: CMP Zero Page X cycles Used: " << cyclesUsed << " != 0" << std::endl;
+    if (cpu._sr._value != 0b00000011) {
+        std::cerr << "FAILURE: CMP Zero Page X: " << std::bitset<8>(cpu._sr._value) << " != 0b00000011" << std::endl;
+    } else
+        std::cout << "SUCCESS: CMP Zero Page X: " << std::bitset<8>(cpu._sr._value) << std::endl;
+
+    // CMP Absolute
+
+    cpu.clear();
+    cpu._memory[0xFFFC]._value = 0x00;
+    cpu._memory[0xFFFD]._value = 0x10;
+    cpu._memory[0x1000]._value = 0xCD;
+    cpu._memory[0x1001]._value = 0x02;
+    cpu._memory[0x1002]._value = 0x20;
+    cpu._memory[0x2002]._value = 0x10;
+
+    cpu.reset();
+    cpu._a.set(0x10);
+
+    if ((cyclesUsed = cpu.execute(4)) != 0)
+        std::cerr << "FAILURE: CMP Absolute cycles Used: " << cyclesUsed << " != 0" << std::endl;
+    if (cpu._sr._value != 0b00000011) {
+        std::cerr << "FAILURE: CMP Absolute: " << std::bitset<8>(cpu._sr._value) << " != 0b00000011" << std::endl;
+    } else
+        std::cout << "SUCCESS: CMP Absolute: " << std::bitset<8>(cpu._sr._value) << std::endl;
+
+    // CMP Absolute X
+
+    cpu.clear();
+    cpu._memory[0xFFFC]._value = 0x00;
+    cpu._memory[0xFFFD]._value = 0x10;
+    cpu._memory[0x1000]._value = 0xDD;
+    cpu._memory[0x1001]._value = 0x02;
+    cpu._memory[0x1002]._value = 0x20;
+    cpu._memory[0x2005]._value = 0x10;
+
+    cpu.reset();
+    cpu._x.set(0x03);
+    cpu._a.set(0x10);
+
+    if ((cyclesUsed = cpu.execute(4)) != 0)
+        std::cerr << "FAILURE: CMP Absolute X cycles Used: " << cyclesUsed << " != 0" << std::endl;
+    if (cpu._sr._value != 0b00000011) {
+        std::cerr << "FAILURE: CMP Absolute X: " << std::bitset<8>(cpu._sr._value) << " != 0b00000011" << std::endl;
+    } else
+        std::cout << "SUCCESS: CMP Absolute X: " << std::bitset<8>(cpu._sr._value) << std::endl;
+
+    // CMP Absolute Y
+
+    cpu.clear();
+    cpu._memory[0xFFFC]._value = 0x00;
+    cpu._memory[0xFFFD]._value = 0x10;
+    cpu._memory[0x1000]._value = 0xD9;
+    cpu._memory[0x1001]._value = 0x02;
+    cpu._memory[0x1002]._value = 0x20;
+    cpu._memory[0x2005]._value = 0x10;
+
+    cpu.reset();
+    cpu._y.set(0x03);
+    cpu._a.set(0x10);
+
+    if ((cyclesUsed = cpu.execute(4)) != 0)
+        std::cerr << "FAILURE: CMP Absolute Y cycles Used: " << cyclesUsed << " != 0" << std::endl;
+    if (cpu._sr._value != 0b00000011) {
+        std::cerr << "FAILURE: CMP Absolute Y: " << std::bitset<8>(cpu._sr._value) << " != 0b00000011" << std::endl;
+    } else
+        std::cout << "SUCCESS: CMP Absolute Y: " << std::bitset<8>(cpu._sr._value) << std::endl;
+
+    // CMP Indirect X
+
+    cpu.clear();
+    cpu._memory[0xFFFC]._value = 0x00;
+    cpu._memory[0xFFFD]._value = 0x10;
+    cpu._memory[0x1000]._value = 0xC1;
+    cpu._memory[0x1001]._value = 0x08;
+    cpu._memory[0x000A]._value = 0x10;
+    cpu._memory[0x000B]._value = 0x20;
+    cpu._memory[0x2010]._value = 0x10;
+
+    cpu.reset();
+    cpu._a.set(0x10);
+    cpu._x.set(0x02);
+
+    if ((cyclesUsed = cpu.execute(6)) != 0)
+        std::cerr << "FAILURE: CMP Indirect X cycles Used: " << cyclesUsed << " != 0" << std::endl;
+    if (cpu._sr._value != 0b00000011) {
+        std::cerr << "FAILURE: CMP Indirect X: " << std::bitset<8>(cpu._sr._value) << " != 0b00000011" << std::endl;
+    } else
+        std::cout << "SUCCESS: CMP Indirect X: " << std::bitset<8>(cpu._sr._value) << std::endl;
+
+    // CMP Indirect Y
+
+    cpu.clear();
+    cpu._memory[0xFFFC]._value = 0x00;
+    cpu._memory[0xFFFD]._value = 0x10;
+    cpu._memory[0x1000]._value = 0xD1;
+    cpu._memory[0x1001]._value = 0x0A;
+    cpu._memory[0x000A]._value = 0x05;
+    cpu._memory[0x000B]._value = 0x20;
+    cpu._memory[0x2007]._value = 0x10;
+
+    cpu.reset();
+    cpu._a.set(0x10);
+    cpu._x.set(0x02);
+
+    if ((cyclesUsed = cpu.execute(5)) != 0)
+        std::cerr << "FAILURE: CMP Indirect Y cycles Used: " << cyclesUsed << " != 0" << std::endl;
+    if (cpu._sr._value != 0b00000011) {
+        std::cerr << "FAILURE: CMP Indirect Y: " << std::bitset<8>(cpu._sr._value) << " != 0b00000011" << std::endl;
+    } else
+        std::cout << "SUCCESS: CMP Indirect Y: " << std::bitset<8>(cpu._sr._value) << std::endl;
     return 0;
 }
